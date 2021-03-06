70bab43d8ec7266109d291ef7dd7a303a1d78f51
Faduma Farah <ffaduma60@gmail.com>
2021-03-05 22:41:19 -0800

    commit

--- SudokuFitness.cpp
+++ SudokuFitness.cpp
@@ -10,8 +10,7 @@ SudokuFitness::howFit(int sudoku[][])
    int conflicts = 0;
    for(int i=0; i<9; i++){
       for(int j=0; j<9; j++){
-         int index = sudoku[i][j];
-         
+         int index = sudoku[i][j]; 
       }
    }
 }
