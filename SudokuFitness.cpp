15fca95c1ae353df6e787f2def7ddc4f5d505ad5
Faduma Farah <ffaduma60@gmail.com>
2021-03-05 22:31:28 -0800

    commit

---
+++ SudokuFitness.cpp
@@ -0,0 +1,17 @@
+#include "SudokuFitness.h"
+
+SudokuFitness::SudokuFitness()
+{
+
+}
+
+SudokuFitness::howFit(int sudoku[][])
+{
+   int conflicts = 0;
+   for(int i=0; i<9; i++){
+      for(int j=0; j<9; j++){
+         int index = sudoku[i][j];
+         
+      }
+   }
+}
