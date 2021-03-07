64f07bbcdc95976a471f2cca8af70d2da3e2e5d7
Faduma Farah <ffaduma60@gmail.com>
2021-03-06 20:47:19 -0800

    Fatima's commit for howFit (need to change 2D array to vector)

--- SudokuFitness.cpp
+++ SudokuFitness.cpp
@@ -11,18 +11,21 @@ int SudokuFitness::howFit(int sudoku[9][9]) const
    int columnConflicts = 0;
    int boxConflicts = 0;
 
-   bool conflictBooleans[9][9];
 
    /*checks conflicts in every row and column*/
    int row = 0;
    while(row<9){
       for(int column=0; column<9; column++){
          for(int index= column + 1; index<9; index++){
-            if(sudoku[row][column] == sudoku[row][index]) {
+            if(sudoku[row][column] == sudoku[row][index]){
                ++rowConflicts;
+               cout << "Point of conflict in row: " << row << ", " << column <<
+                " and " << row << ", " << index << endl;
             }
             if(sudoku[column][row] == sudoku[index][row]){
                ++columnConflicts;
+               cout << "Point of conflict in column: " << column << ", " << row
+                << " and " << index << ", " << row << endl;
             }
          }
       }
@@ -30,8 +33,7 @@ int SudokuFitness::howFit(int sudoku[9][9]) const
    }
 
    for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 3; j++)
-		{
+		for (int j = 0; j < 3; j++){
 			boxConflicts += calculateBoxConflicts(i*3, j*3, sudoku);  
 		}
 	}
@@ -42,25 +44,27 @@ int SudokuFitness::howFit(int sudoku[9][9]) const
    return rowConflicts + columnConflicts + boxConflicts;
 }
 
-int SudokuFitness::calculateBoxConflicts(int startXPoint, int startYPoint, int TwoDArray[9][9]) const
+int SudokuFitness::calculateBoxConflicts(int startXPoint, int startYPoint, 
+   int sudoku[9][9]) const
 {
-   int boxConflicts;
+   int boxConflicts = 0;
 
    /*get all values in a box and puts in array*/
-   int values[9];
-   int value = 0;
+   vector<int> values;
 	for (int i = 0; i< 3; i++){
 		for (int j = 0; j < 3; j++){
-			values[value] = TwoDArray[startXPoint + i][startYPoint + j];   
-			++value;
-		}
+         values.push_back(sudoku[startXPoint + i][startYPoint + j]);
+      }
    }
 
    /*calculates conflicts in box*/
    for (int i = 0; i < 9; i++){
       for (int j = i+1; j < 9; j++){
-         if(values[i] == values[j]) {
+         if(values[i] == values[j]){
 				++boxConflicts;
+            cout << "Points of conflict for box: " << "Starting point: " << 
+               startXPoint << ", " << startYPoint << endl;
+            cout << "conflict: value " << i << ", value " << j << endl;
 			}
       }
 	}
