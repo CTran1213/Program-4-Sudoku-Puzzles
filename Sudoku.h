a6a4a2a0c19ad6e81eb12bb54c268dfe77344618
Catherine Tran <72372224+CTran1213@users.noreply.github.com>
2021-03-11 01:26:54 -0800

    Update Sudoku.h

--- Sudoku.h
+++ Sudoku.h
@@ -1,3 +1,11 @@
+//Sudoku.h
+//3/11/2021
+//@author: Faduma Farah
+//@studentID: 1727406
+//Catherine Tran ID: 2021829
+//The Sudoku class must hold the representation of a Sudoku puzzle, meaning it must know which numbers in the 
+//9-by-9 grid are fixed (the ones given at the start of the puzzle) and which are variable (the ones written 
+//in as a possible solution).
 #pragma once
 #include<iostream>
 #include "Puzzle.h"
@@ -8,6 +16,8 @@ using namespace std;
 
 class Sudoku : public Puzzle
 {
+	//This istream operator takes in a string of characters and uses the first 81 digits to populate the sudoku grid. 
+	//Digits that are 0 are not fixed numbers and digits that are 1-9 are fixed numbers
 	friend istream& operator>>(istream& instream, Sudoku& sudoku)
    {
       int count = 0;
@@ -33,6 +43,12 @@ class Sudoku : public Puzzle
       return instream;
    }
 
+	//This ostream operator produces a "human friendly" output as formatted text. 
+	//Output a puzzle as 13 lines of text, with each line either being a separator or 
+	//one containing the numbers in a row of the puzzle separated by single spaces. Use the 
+	//'|', '+', and '-' characters to separate 3-by-3 blocks (so, each line of output should be 25 
+	//printing characters long: 9 digits, 12 spaces, and 4 '|' for "non-divider" lines and 21 '-' 
+	//and 4 '+' for "divider" lines). An example (very simple puzzle) is shown below.
 	friend ostream& operator<<(ostream& outstream, const Sudoku& sudoku)
    {
       outstream << "+-------+-------+-------+" << endl;
@@ -55,16 +71,34 @@ class Sudoku : public Puzzle
    }
 
 public:
+   //Constructor. Resizes the vector to hold a 9-by-9 grid. Makes every fixed value false.
    Sudoku();
    //~Sudoku();
    int getFitness() const;
    bool setFitnessNumber(int num);
+   
+   //Returns the 9-by-9 vector. Is used in other classes to change or look at each box
+   //Precondition: none
+   //Postcondition: the sudoku is initialized and the vector is returned
    vector<vector<int>> getSudokuArray() const;
+   
+   //Returns a boolean letting the user know if the box at the specified row and column is fixed or not
+   //Precondition: the sudoku is initialized
+   //Postcondition: returns true if the box is fixed, false otherwise
    bool isFixed(int row, int column) const;
+   
+   //This method sets the value of the specified box
+   //Precondition: None
+   //Postcondition: The number at row,column is changed. returns true
    bool setSudokuBoxValue(int num, int row, int column);
 
 private:
+   //A 2D array that is the sudoku puzzle. It is a 9-by-9 grid that holds integers
    vector<vector<int>> sudokuNumbers_;
+   
+   //9-by-9 grid that holds booleans. Each box corresponds to a box in sudokuNumber_. 
+   //True means the number is fixed and cannot be change, while false means the number 
+   //isn't fixed and can be changed
    vector<vector<bool>> fixedValues_;
    int fitnessNumber_;
 };
