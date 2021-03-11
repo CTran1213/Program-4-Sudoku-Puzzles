16b24f8ba526e02a5270a33f5da24af9ab7def41
Catherine Tran <cathy.tran836@gmail.com>
2021-03-11 15:39:12 -0800

    comments

--- SudokuOffspring.cpp
+++ SudokuOffspring.cpp
@@ -1,3 +1,12 @@
+//SudokuFitness.cpp
+//3/12/2021
+//@author: Faduma Farah
+//@studentID: 1727406
+//Catherine Tran ID: 2021829
+//Implementation of SudokuOffspring.h
+//The SudokuFitness class implements "makeOffspring" for Puzzle objects. It uses the 
+//mutation operator from genetic programming to create a copy of the given Puzzle 
+//which has a 3% chance of mutation
 #include "SudokuOffspring.h"
 
 SudokuOffspring::SudokuOffspring()
@@ -5,11 +14,12 @@ SudokuOffspring::SudokuOffspring()
 
 }
 
+//
 Puzzle*& SudokuOffspring::makeOffspring(Puzzle*& sudokuPuzzle) 
 {
    double val; //Random number used to get a 5% probability
    int numChange; //A number between and including 1-9
-
+   
    Sudoku& sudoku = *(static_cast<Sudoku * >(sudokuPuzzle));
 
    vector<vector<int>> tempSudoku = sudoku.getSudokuArray();
@@ -17,16 +27,18 @@ Puzzle*& SudokuOffspring::makeOffspring(Puzzle*& sudokuPuzzle)
    //iterate through the rows
    for (int i = 0; i < 9; i++)
    {
+      //Invariant: There are still rows of digits to look at
       //iterate through the columns
       for (int j = 0; j < 9; j++)
       {
+         //Invariant: There are still columns of digits to look at
          val = ((double)rand())/ (RAND_MAX + 1.0); //Random number between 0 and 1
          numChange = rand()%9 + 1; //Random number between and including 1-9
          if (tempSudoku[i][j] == 0) //Sets the box to a random number [1,9] if it is empty
          {
             sudoku.setSudokuBoxValue(numChange, i, j);
          }
-         else if (val < 0.05 && !sudoku.isFixed(i,j)) //5%
+         else if (val < 0.03 && !sudoku.isFixed(i,j)) //3%
          {
             sudoku.setSudokuBoxValue(numChange, i, j); //Changes the value if the box is not fixed AND val is less than .05 (5%)
          }
