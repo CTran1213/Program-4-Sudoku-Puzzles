ea475374eeec20dbd3b01a6ce7f4a309fbb06616
Catherine Tran <72372224+CTran1213@users.noreply.github.com>
2021-03-11 00:42:47 -0800

    Update Fitness.h

--- Fitness.h
+++ Fitness.h
@@ -1,3 +1,13 @@
+//Fitness.h
+//3/12/2021
+//@author: Faduma Farah
+ //@studentID: 1727406
+//Catherine Tran ID: 2021829
+//Fitness is used by a Population to evaluate individual Puzzles.
+//Fitness and its subclasses are implementations of the strategy design pattern.
+//Fitness subclasses must implement a "howFit" method, which takes a Puzzle (subclass) object and returns an int. 
+//Perhaps counter-intuitively, lower fitness values indicate more fit individuals (in other words, howFit is indicating how far a Puzzle is from perfection).
+
 #pragma once 
 #include <vector>
 #include "Puzzle.h"
