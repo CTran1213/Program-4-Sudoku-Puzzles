8feaa4933e632453660352fceee6ad9f3a6b31a2
Catherine Tran <72372224+CTran1213@users.noreply.github.com>
2021-03-11 01:51:06 -0800

    Update SudokuFactory.cpp

--- SudokuFactory.cpp
+++ SudokuFactory.cpp
@@ -1,11 +1,24 @@
+//SudokuFactory.cpp
+//3/12/2021
+//@author: Faduma Farah
+//@studentID: 1727406
+//Catherine Tran ID: 2021829
+//Implementation of SudokuFactory.h
+//The SudokuFactory uses a SudokuOffspring object and implements 
+//"createPuzzle" to produce new Puzzle objects.
 #include "SudokuFactory.h"
 
+//Constructor. Accepts a reproduction object and turns it 
+//into a SudokuOffspring object. This is used to call the method "makeOffspring"
 SudokuFactory::SudokuFactory(Reproduction*& offspringCreator)
 {
    SudokuOffspring* temp = (SudokuOffspring *) offspringCreator;
    this->offspringCreator_ = temp;
 }
 
+//This method sses the offspringCreator data member to create and return a new puzzle
+//Precondition: None
+//Postcondition: Returns a puzzle created from the sudokuoffspring object
 Puzzle*& SudokuFactory::createPuzzle(Puzzle*& sudokuPuzzle)
 {
    this->offspringCreator_->makeOffspring(sudokuPuzzle);
