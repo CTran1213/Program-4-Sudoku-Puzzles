ced2ba9897cafb267094b894c138d576ce252d62
Catherine Tran <72372224+CTran1213@users.noreply.github.com>
2021-03-11 01:51:44 -0800

    Update SudokuFactory.h

--- SudokuFactory.h
+++ SudokuFactory.h
@@ -1,3 +1,10 @@
+//SudokuFactory.h
+//3/11/2021
+//@author: Faduma Farah
+//@studentID: 1727406
+//Catherine Tran ID: 2021829
+//The SudokuFactory uses a SudokuOffspring object and implements 
+//"createPuzzle" to produce new Puzzle objects.
 #pragma once
 #include "Puzzle.h"
 #include "PuzzleFactory.h"
@@ -7,9 +14,16 @@
 class SudokuFactory : public PuzzleFactory
 {
    public:
+      //Constructor. Accepts a reproduction object and turns it 
+      //into a SudokuOffspring object. This is used to call the method "makeOffspring"
       SudokuFactory(Reproduction*& offspringCreator);
+      
+      //This method sses the offspringCreator data member to create and return a new puzzle
+      //Precondition: None
+      //Postcondition: Returns a puzzle created from the sudokuoffspring object
       Puzzle*& createPuzzle(Puzzle*& sudokuPuzzle);
 
    private:
+      //This object is used to create new mutated puzzles. It is used in the createPuzzle method
       SudokuOffspring* offspringCreator_;
 };
